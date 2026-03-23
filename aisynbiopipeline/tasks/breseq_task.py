855575d4f41cb197da5b87dcd049f90c398712e7
nspahr08 <nspahr08@gmail.com>
2026-03-23 19:29:06 +0000

    Updated environment.yml with latest requirements.

--- aisynbiopipeline/tasks/breseq_task.py
+++ aisynbiopipeline/tasks/breseq_task.py
@@ -33,7 +33,7 @@ app.conf.update(
     timezone='UTC',
     enable_utc=True,
     task_track_started=True,
-    task_time_limit=36000,  # 10 hour default timeout 18000,  # 5 hour default timeout 10800,  # 3 hour default timeout
+    task_time_limit=86400,  # 24 hour default timeout 18000,  # 5 hour default timeout 10800,  # 3 hour default timeout
     worker_prefetch_multiplier=1,
     result_expires=86400,  # Results expire after 24 hours
 
